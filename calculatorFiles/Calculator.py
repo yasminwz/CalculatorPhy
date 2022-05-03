8cb514c78fcd223284d80b702257e23029227e05
yasminwz <yas.desenv@gmail.com>
2022-05-03 15:26:40 -0300

    first commit

---
+++ calculatorFiles/Calculator.py
@@ -0,0 +1,27 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print("Choise an operation")
+print("1 = sum")
+print("2 = subtraction")
+print("3 = division")
+print("4 = multiplication")
+
+operation = int((input("Enter the operation: ")))
+
+if operation == 1:
+    sum = int(num1 + num2)
+    print("The result is:", sum)
+
+if operation == 2:
+    subtracao = int(num1 - num2)
+    print("The result is:")
+
+if operation == 3:
+    div = int(num1 / num2)
+    print("The result is:", div)
+
+if operation == 4:
+    mult = int(num1 * num2)
+    print("The result is:", mult)
+
